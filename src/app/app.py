b2c2d39cc0fa4badcf00ac1ed4145c4c5bb646f4
idmaksim <i@dmaksim.ru>
2024-05-15 20:32:59 +0300

    commit

--- src/app/app.py
+++ src/app/app.py
@@ -2,8 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-# from mycardb.db import create_db_and_tables
-from src.db.db import create_db_and_tables
+
+from db.db import create_db_and_tables
 from api.routers import main_api_router
 
 
