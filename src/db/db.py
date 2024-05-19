bdc2215957d9b5c2b8fc298e4f4641db15260a88
idmaksim <i@dmaksim.ru>
2024-05-19 15:20:27 +0300

    commit

--- src/db/db.py
+++ src/db/db.py
@@ -1,7 +1,5 @@
-from typing import AsyncGenerator
-
 from sqlalchemy import Column, Integer
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 
