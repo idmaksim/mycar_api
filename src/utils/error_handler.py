ba062710d29a79e3d26b9318b06bcdab3d0afb78
idmaksim <i@dmaksim.ru>
2024-05-30 14:19:36 +0300

    commit

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -2,4 +2,6 @@ from fastapi import HTTPException, status
 
 
 async def handle_route_error(e: Exception, status_code: status):
+    print(str(e))
+    print(e)
     raise HTTPException(detail=str(e), status_code=status_code)
