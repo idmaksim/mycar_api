746e0a8405916ba186bf459658788fe51c91cb66
idmaksim <i@dmaksim.ru>
2024-06-10 13:55:26 +0300

    commit

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, status
+from fastapi.encoders import jsonable_encoder
 
 
 async def handle_route_error(e: Exception, status_code: status):
-    print(e)
-    raise HTTPException(detail=str(e), status_code=status_code)
+    raise HTTPException(detail=jsonable_encoder(e), status_code=status_code)
